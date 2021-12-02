210f9614e2d280335a380c2f610ee40065ffaccc
Raul Rodriguez <raul0882@gmail.com>
2021-12-01 20:53:24 -0500

    first commit

---
+++ Bank_Atm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Bank_Atm.Database;
+
+
+namespace Bank_Atm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creates database connection
+            CreateDatabase connect = new CreateDatabase();
+            connect.DataBaseConnect();
+
+
+        }
+    }
+}
